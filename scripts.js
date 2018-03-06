77267ea1ec7178df74697977770b91199d47ad1f
Zhenzhen You <swimreally@MacBook-Air.local>
2018-03-06 11:06:38 -0800

    add navbar and hamburger icon

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
-  var btn = document.getElementById("myBtn");
-  <!-- bitton fnct goes here -->
+    $('.animated-icon1,.animated-icon3,.animated-icon4').click(function(){
+        $(this).toggleClass('open');
+    });
 });
