79b1b5a223cfedade9dbdeac90e3b13d4fe6de2a
Zhenzhen You <swimreally@MacBook-Air.local>
2018-03-08 15:40:08 -0800

    positioned the footer, and blurred the video

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,5 +11,27 @@ $(document).ready(function(){
     $('.heartRed').click(function() {
         $(this).toggle();
         $(this).siblings().toggle();
-    })
+    });
+
+    var blurloaded = false;
+    var vidloaded = false;
+
+    myVid = document.getElementById('myVideoBlur');
+    myVid.onloadeddata = playVideos('blur');
+
+    myVid = document.getElementById('myVideo');
+    myVid.onloadeddata = playVideos('video');
+
+    function playVideos(video) {
+        if (video == 'blur') {
+            blurloaded = true;
+        } else if (video == 'video') {
+            vidloaded = true;
+        }
+        
+        if (blurloaded && vidloaded) {
+            document.getElementById('myVideo').play();
+            document.getElementById('myVideoBlur').play();
+        }
+    }
 });
